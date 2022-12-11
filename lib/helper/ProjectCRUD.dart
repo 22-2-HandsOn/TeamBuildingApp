87f9ec4a5111730536aa6a2c751bc1603feeada6
mjk8 <mjktt8@gmail.com>
2022-12-12 00:13:05 +0900

    [Add] comment methods

--- lib/helper/ProjectCRUD.dart
+++ lib/helper/ProjectCRUD.dart
@@ -27,6 +27,41 @@ class ProjectCRUD{
     }
   }
 
+  Future addComment(String content, bool isSecret) async{
+    FirebaseFirestore.instance.collection("comments").add({});
+    var stu_id = await getstu_id();
+    var timeZoneOffset = DateTime.now().timeZoneOffset.inMilliseconds;
+    final QuerySnapshot snapshot = await attendeesCollection.get();
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>;
+      if (dataElement['stu_id'].toString() == stu_id) {
+        final a = await attendeesCollection.doc(doc.id).collection('comments').doc().set({
+          'author_doc_id': doc.id,
+          'isSecret': isSecret,
+          'content': content,
+          'timestamp': DateTime.now().millisecondsSinceEpoch + timeZoneOffset,
+        });
+      }
+    }
+  }
+
+  Future getComment() async{
+    var stu_id = await getstu_id();
+    var attendee_id = await getAttendeeID();
+    List<String> data = [];
+    final QuerySnapshot snapshot = await attendeesCollection.get();
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>;
+      if (dataElement['stu_id'].toString() == stu_id) {
+        QuerySnapshot snapshot2 = await attendeesCollection.doc(doc.id).collection('comments').get();
+        for (var doc2 in snapshot2.docs) {
+          data.add(doc2.data().toString());
+        }
+      }
+    }
+    return data;
+  }
+
   Future getTeamInfo() async {
     final snapshot = await teamsCollection.get();
     var stu_id = await getstu_id();
@@ -131,6 +166,17 @@ class ProjectCRUD{
         if (dataElement[i].toString() == stu_id) {
           teamsCollection.doc(doc.id).update({'name': intro});
         }
+      }
+    }
+  }
+
+  Future getattendeedoc(String comment) async{
+    var stu_id = await getstu_id();
+    final QuerySnapshot snapshot = await attendeesCollection.get();
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>;
+      if (dataElement['stu_id'].toString() == stu_id) {
+        return doc.id;
       }
     }
   }
